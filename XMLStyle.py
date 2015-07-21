cf5fe98b92c2aa6e46c24567ca1ef792e01ebb18
rdawis <rdawis@ucsd.edu>
2015-07-20 20:18:57 -0500

    [Mostly Complete] Test XML Structure [In Progress] PetriNet Interpreter NEW: Recursion works! :D the SVG output finally kinda works, just needs prettifying, and the x/y coords need to be dealt with. Hopefully this won't be an issue (it's easy, and maybe if I'm cool enough, I won't need to do this) MORE NEW: [Crudely Working?] Stylesheet

--- XMLStyle.py
+++ XMLStyle.py
@@ -1,28 +1,28 @@
 ## IMPORTS ##
 
-from lxml import etree  # Need to process XML
-import libxslt          # May not be necessary
+from lxml import etree,html  # Need to process XML
+from BeautifulSoup import BeautifulSoup as bs
 
 ## variables ##
 
-html = \
+html_text = \
     '''
 <!DOCTYPE html>
 <html>
     <head>
         <!--link rel='stylesheet' href='styles.css' /-->
     </head>
-    <svg viewBox='0 0 1920 1080' height='100%' width='100%'>
+    <svg viewBox='0 0 1920 1080' height='100%%' width='100%%'>
         %s
     </svg>
 </html>
     '''
 svg_text = \
-    ('<svg height = \'12.5%\' width = \'12.5%\' x = \'%s\' y = \'%s\'>\n'
+    ('<svg height = \'12.5%%\' width = \'12.5%%\' x = \'%s\' y = \'%s\'>\n'
      '    %s\n'
      '    %s\n'
      '</svg>\n')
-print svg_text
+#print svg_text
 xml_file = 'test_net.xml'
 tree = etree.parse(xml_file)
 ## Definitions ##
@@ -34,31 +34,42 @@ def attrScanner(parent,svg_format):
     Might be helpful for generating shapes.
     Can use for extraction of x,y relative coordinates.
     """
+
     tf_scan = '%s/f | %s/t'
-    max_length = len(tree.xpath(tf_scan % (parent,parent)))
-    new_parents = []
+    max_length=len(tree.xpath(tf_scan % (parent,parent)))
+    svg = ''
     for i in range(max_length):
-        child = tree.xpath(tf_scan % (parent,parent))[i].tag
+        child_tag = tree.xpath(tf_scan % (parent,parent))[i].tag
         child_id = tree.xpath(tf_scan % (parent,parent))[i].get('id')
         x_id = tree.xpath(tf_scan % (parent,parent))[i].get('x')
         y_id = tree.xpath(tf_scan % (parent,parent))[i].get('y')
-        new_parents = '%s/%s[@id=\'%s\']' % (parent,child,child_id)
-        svg += svg_format % (x_id,y_id,shapeChoose(child),'%s')
-    for i in range(max_length):
-        attrScanner(new_parents[i],svg_format)
+        new_parent = '%s/%s[@id=\'%s\']' % (parent,child_tag,child_id)
+        #print shapeChoose(child_tag)
+        #print x_id
+        #print y_id
+        #print new_parent
+        #print svg_format % (x_id,y_id,shapeChoose(child_tag),attrScanner(new_parent,svg_format))
+        svg += svg_format % (x_id,y_id,shapeChoose(child_tag),attrScanner(new_parent,svg_format))
+
+    return svg
 
 def xmlTemplate():
     pass
 
 def shapeChoose(element):
     if element == 'f':
-        svg_text = '''<ellipse cx='50%' cy='50%' rx='49%' ry='49%' />'''
+        svg_text = '<ellipse cx=\'50%\' cy=\'50%\' rx=\'49%\' ry=\'49%\' />'
     elif element == 't':
-        svg_text = '''<rect x='1%' y='1%' width='98%' height='98%' />'''
+        svg_text = '<rect x=\'1%\' y=\'1%\' width=\'98%\' height=\'98%\' />'
     #elif element == 'w':
     #    drawWire(element)
+    return svg_text
 
 def drawWire(element):
     element
 
-attrScanner("/petrinet",svg_text)
+print attrScanner("/petrinet",svg_text)
+
+#full_svg = pretty_svg.prettify()
+print '########################################'
+print unpretty_svg
