0ae5b7ae6c4061ac1e839badb0405cf5ede40463
rdawis <rdawis@ucsd.edu>
2015-07-22 17:41:46 -0500

    Django: Models are pretty much set, added Unit and Connection objects, with Unit objects differentiable by shape. TO DO: MODELS: add verification stuffs such that     - parents can't connect to children,     - forms can only connect to transforms et v.v.     - elements that interconnect to elements of other parents must:         -be the same         -be the same class element as the parent VIEWS: add templates Controller: ???

--- PetriNet/PetriNet/urls.py
+++ PetriNet/PetriNet/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^objectify/', include('objectify.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
