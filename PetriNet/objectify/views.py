6f047463166296f19add4d7435f994686e031c82
rdawis <rdawis@ucsd.edu>
2015-07-23 16:07:50 -0500

    Django: NEW: Models remodeled to use MPTT in order to make recursing easier and to maintain parent child relationships NO MORE BROKEN FAMILIES Still need to work on wiring system, probably will reimplement parent/child wire relationships. ADDED VIEW/TEMPLATE (finally) (gonna check if that works laters) (still need) TO DO: MODELS: add verification stuffs such that     - parents can't connect to children,     - forms can only connect to transforms et v.v.     - elements that interconnect to elements of other parents must:         -be the same         -be the same class element as the parent VIEWS: add templates Controller: ???

--- PetriNet/objectify/views.py
+++ PetriNet/objectify/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View
+from models import Unit
+
 # Create your views here.
+class Graph(View):
+    def get(self,request,*args,**kwargs):
+        return HttpResponse('hallo werldpool')
+
+def show_pnet(request):
+    return render_to_response("pnet.html",
+                          {'nodes':Unit.objects.all()},
+                          context_instance=RequestContext(request))
+
 def index(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("<h1>Hello World!</h1>")
